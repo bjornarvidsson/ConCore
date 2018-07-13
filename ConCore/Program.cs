be5b76625e200d6a3f6b1ac31f0b958d0b5682df
Björn Arvidson <bjar@synsam.com>
2018-07-13 15:49:46 +0200

    confirm exit

---
+++ ConCore/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Press x to exit.");
+            var key = Console.ReadKey();
+            while (key.KeyChar != 'x' )
+            {
+
+                key = Console.ReadKey();
+            }
+            Console.WriteLine("Are you sure you want to exit? (y/n)");
+            key = Console.ReadKey();
+            while (key.KeyChar != 'y')
+            {
+                key = Console.ReadKey();
+            }
+
+        }
+    }
+}
